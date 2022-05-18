3e025527d040113ce452d5fe5a17da85a9b80192
darshanpandey <88545777+darshanpandey@users.noreply.github.com>
2022-05-18 11:04:01 +0530

    Add files via upload

---
+++ js/Sketch.js
@@ -0,0 +1,14 @@
+var  foodSound = new Audio('music/food.mp3');
+var gameOverSound = new Audio('music/gameover.mp3');
+var moveSound = new Audio('music/move.mp3');
+var musicSound = new Audio('music/music.mp3');
+var speed = 19;
+var score = 0;
+var  lastPaintTime = 0;
+var snakeArr = [
+    {x: 13, y: 15}
+];
+
+food = {x: 6, y: 7};
+
+
